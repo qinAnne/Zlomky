c6753184d5f95502ed572e366b2423b957ce1301
ctvrtan1 <ctvrteckova.anicka@gmail.com>
2025-02-17 11:27:42 +0100

    Arithmetic operations on Zlomek

--- src/main/java/cz/uhk/zlomky/model/Zlomek.java
+++ src/main/java/cz/uhk/zlomky/model/Zlomek.java
@@ -14,7 +14,35 @@ public class Zlomek {
     public int getJmenovatel(){
         return jmenovatel;
     }
+    public Zlomek krat(Zlomek druhy){
+        return new Zlomek(citatel*druhy.citatel, jmenovatel*druhy.jmenovatel);
+    }
+    public Zlomek deleno(Zlomek druhy){
+        return new Zlomek(citatel*druhy.jmenovatel, jmenovatel*druhy.citatel);
+    }
+    public Zlomek plus(Zlomek druhy){
+        return new Zlomek ((citatel*druhy.jmenovatel) + (druhy.citatel*jmenovatel), jmenovatel*druhy.jmenovatel);
+    }
+    public Zlomek minus(Zlomek druhy){
+        return new Zlomek((citatel*druhy.jmenovatel) - (druhy.citatel*jmenovatel), jmenovatel*druhy.jmenovatel);
+    }
+    public Zlomek zkratit(){
+        int a = Math.abs(citatel);
+        int b = Math.abs(jmenovatel);
+        if(a<b){
+            int pom = a;
+            a = b;
+            b = pom;
+        }
 
+        int zb;
+        do{
+            zb = a%b;
+            a = b;
+            b= zb;
+        } while(zb!=0);
+        return new Zlomek(citatel/a, jmenovatel/ a);
+    }
     @Override
     public String toString() {
         return String.format("%d/%d", citatel, jmenovatel);
--- src/main/java/zlomkyApp.java
+++ src/main/java/zlomkyApp.java
@@ -7,5 +7,12 @@ public class zlomkyApp {
         //vytvoření instance(=objektu) tridy Zlomek
         Zlomek a = new Zlomek(3,4);
         System.out.printf("Zlomek a =%s\n", a.toString());
+
+        Zlomek b = new Zlomek(2,5);
+        Zlomek c = a.krat(b);
+        System.out.printf("Zlomek a*b = %s\n", c.zkratit());
+        System.out.printf("Zlomek a/b = %s\n", a.deleno(b));
+        System.out.printf("Zlomek a+b = %s\n", a.plus(b));
+        System.out.printf("Zlomek a-b = %s\n", a.minus(b));
     }
 }
